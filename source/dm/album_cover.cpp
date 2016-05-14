7d372deba61816e1ec358c473a85957544d5eb1f
Benny Bach <benny.bach@gmail.com>
2016-05-14 15:06:49 +0200

    Add album_cover class

---
+++ source/dm/album_cover.cpp
@@ -0,0 +1,80 @@
+// ----------------------------------------------------------------------------
+//
+//     Author     : Benny Bach <benny.bach@gmail.com>
+//                  Copyright (C) 2015
+//
+// ----------------------------------------------------------------------------
+#include "album_cover.h"
+
+// ----------------------------------------------------------------------------
+namespace musicbox
+{
+  album_cover::album_cover() : mime_type_(), data_()
+  {
+  }
+
+  const std::string& album_cover::mime_type() const
+  {
+    return mime_type_;
+  }
+
+  const std::string& album_cover::data() const
+  {
+    return data_;
+  }
+
+  void album_cover::mime_type(const std::string& mime_type)
+  {
+    mime_type_ = mime_type;
+  }
+
+  void album_cover::data(const unsigned char* buf, size_t len)
+  {
+    data_.assign(reinterpret_cast<const char*>(buf), len);
+  }
+
+  void album_cover::read(msgpack::istream& is)
+  {
+    msgpack::map map;
+
+    if ( is >> map )
+    {
+      for ( size_t i=0; i<map.size(); i++ )
+      {
+        unsigned key;
+
+        if ( is >> key )
+        {
+          switch ( key )
+          {
+            case 1: is >> mime_type_; break;
+            case 2: is >> data_; break;
+            default:
+              //is >> msgpack::skip;
+              break;
+          }
+        }
+        else
+        {
+          // ERROR!
+          std::cout << "failed to read album key" << std::endl;
+        }
+      }
+    }
+    else
+    {
+      // ERROR!
+      std::cout << "failed to read album map" << std::endl;
+    }
+
+  }
+
+  void album_cover::write(msgpack::ostream& os) const
+  {
+    msgpack::map map{2};
+
+    os << map
+      << 1 << mime_type_
+      << 2 << data_;
+  }
+}
---
+++ source/dm/album_cover.h
@@ -0,0 +1,54 @@
+// ----------------------------------------------------------------------------
+//
+//     Author     : Benny Bach <benny.bach@gmail.com>
+//                  Copyright (C) 2015
+//
+// ----------------------------------------------------------------------------
+#ifndef __musicbox__dm__album_cover_h__
+#define __musicbox__dm__album_cover_h__
+
+// ----------------------------------------------------------------------------
+#include <msgpack/istream.h>
+#include <msgpack/ostream.h>
+#include <msgpack/map.h>
+#include <msgpack/array.h>
+
+// ----------------------------------------------------------------------------
+#include <string>
+
+// ----------------------------------------------------------------------------
+namespace musicbox
+{
+  class album_cover
+  {
+  public:
+    album_cover();
+  public:
+    const std::string& mime_type() const;
+    const std::string& data() const;
+  public:
+    void mime_type(const std::string&);
+    void data(const unsigned char*, size_t);
+  public:
+    void read(msgpack::istream&);
+    void write(msgpack::ostream&) const;
+  private:
+    std::string mime_type_;
+    std::string data_;
+  };
+
+  inline msgpack::istream& operator>>(msgpack::istream& is, album_cover& value)
+  {
+    value.read(is);
+    return is;
+  }
+
+  inline msgpack::ostream& operator<<(msgpack::ostream& os, const album_cover& value)
+  {
+    value.write(os);
+    return os;
+  }
+}
+
+// ----------------------------------------------------------------------------
+#endif

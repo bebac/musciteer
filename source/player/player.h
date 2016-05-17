8f508956ba4c6a2432434ab60c8fccbcf2e39a0e
Benny Bach <benny.bach@gmail.com>
2016-05-17 19:36:08 +0200

    Separate player and player_impl source

--- source/player/player.h
+++ source/player/player.h
@@ -3,15 +3,12 @@
 //     Author     : Benny Bach <benny.bach@gmail.com>
 //                  Copyright (C) 2015
 //
-// --- Description: -----------------------------------------------------------
-//
-//
 // ----------------------------------------------------------------------------
 #ifndef __player__player_h__
 #define __player__player_h__
 
 // ----------------------------------------------------------------------------
-#include "audio_output.h"
+#include "player_impl.h"
 
 // ----------------------------------------------------------------------------
 #include <memory>
@@ -22,26 +19,6 @@
 // ----------------------------------------------------------------------------
 namespace musicbox
 {
-  class player_impl
-  {
-  public:
-    player_impl(dripcore::loop* loop);
-  public:
-    void subscribe(message_channel ch);
-    void unsubscribe(message_channel ch);
-  public:
-    void audio_device_list(message_channel reply_ch);
-    void audio_device(const std::string& device_name);
-  public:
-    void play(const std::string& id);
-  private:
-    dripcore::loop* loop_;
-    std::string output_device_;
-    audio_output_alsa audio_output_;
-  private:
-    static constexpr const char* output_device_key = "__output_device__";
-  };
-
   class player
   {
   public:
---
+++ source/player/player_impl.cpp
@@ -0,0 +1,100 @@
+// ----------------------------------------------------------------------------
+//
+//     Author     : Benny Bach <benny.bach@gmail.com>
+//                  Copyright (C) 2015
+//
+// --- Description: -----------------------------------------------------------
+//
+//
+// ----------------------------------------------------------------------------
+#include "player.h"
+#include "player_task_flac.h"
+
+// ----------------------------------------------------------------------------
+#include "../dm/tracks.h"
+
+// ----------------------------------------------------------------------------
+#include "../storage/kvstore.h"
+
+// ----------------------------------------------------------------------------
+namespace musicbox
+{
+  player_impl::player_impl(dripcore::loop* loop)
+    : loop_(loop), output_device_()
+  {
+    auto kvstore = musicbox::kvstore();
+
+    kvstore.get(output_device_key, output_device_);
+  }
+
+  void player_impl::subscribe(message_channel ch)
+  {
+    message req(message::subscribe_id, 0);
+
+    req.subscribe.channel = ch;
+
+    audio_output_.send(std::move(req));
+  }
+
+  void player_impl::unsubscribe(message_channel ch)
+  {
+    message req(message::unsubscribe_id, 0);
+
+    req.subscribe.channel = ch;
+
+    audio_output_.send(std::move(req));
+  }
+
+  void player_impl::audio_device_list(message_channel reply_ch)
+  {
+    message req(message::device_list_req_id, 0);
+
+    req.device_list_req.current = output_device_;
+    req.device_list_req.reply = reply_ch;
+
+    audio_output_.send(std::move(req));
+  }
+
+  void player_impl::audio_device(const std::string& device_name)
+  {
+    auto kvstore = musicbox::kvstore();
+
+    output_device_ = device_name;
+
+    kvstore.set(output_device_key, output_device_);
+  }
+
+  void player_impl::play(const std::string& id)
+  {
+    {
+      message req(message::open_req_id, 0);
+
+      req.open_req.device_name = output_device_;
+      //m.open_req.reply = output_channel_;
+      audio_output_.send(std::move(req));
+    }
+
+    {
+      message req(message::play_req_id, 0);
+
+      auto tracks = musicbox::tracks();
+
+      auto track = tracks.find_by_id(id);
+      auto sources = track.sources();
+
+      if ( sources.size() > 0 )
+      {
+        auto filename = sources[0].uri();
+
+        req.play_req.stream_id = 1;
+        req.play_req.uri = filename;
+
+        loop_->spawn<player_task_flac>(req.play_req, audio_output_);
+      }
+      else
+      {
+        std::cout << "no source!" << std::endl;
+      }
+    }
+  }
+}

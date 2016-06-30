288be9b4d87c6eba2fb5cedca8ef989bd20e729a
Benny Bach <benny.bach@gmail.com>
2016-06-30 18:32:35 +0200

    Update player/sources

--- source/player/sources.cpp
+++ source/player/sources.cpp
@@ -11,4 +11,55 @@ namespace musicbox
 {
   sources::source_ptr sources::source_local_;
   sources::source_ptr sources::source_spotify_;
+
+  void sources::play(std::shared_ptr<player_session> session)
+  {
+    auto track = session->track();
+    assert(track);
+
+    auto sources = track->sources();
+
+    if ( sources.size() > 0 )
+    {
+      auto& source = sources[0];
+
+      if ( source.name() == "local" ) {
+        source_local_->send(std::move(session));
+      }
+      else if ( source.name() == "spotify" ) {
+        source_spotify_->send(std::move(session));
+      }
+      else {
+        // TODO: Error!
+      }
+    }
+    else
+    {
+      // TODO: Error!
+    }
+  }
+
+  void sources::start(dripcore::loop* loop)
+  {
+    std::cerr << "starting local source" << std::endl;
+    source_local_.reset(new source_local(loop));
+    source_local_->start();
+
+    musicbox::dm::source_spotify spotify_settings{};
+
+    auto& username = spotify_settings.username();
+    auto& password = spotify_settings.password();
+
+    if ( !username.empty() && !password.empty() )
+    {
+      std::cerr << "starting spotify source" << std::endl;
+      source_spotify_.reset(new source_spotify(loop));
+      source_spotify_->start();
+    }
+    else
+    {
+      std::cerr << "spotify source not configured" << std::endl;
+    }
+  }
+
 }
--- source/player/sources.h
+++ source/player/sources.h
@@ -24,55 +24,9 @@ namespace musicbox
   {
     using source_ptr = std::shared_ptr<source>;
   public:
-    void play(std::shared_ptr<player_session> session)
-    {
-      auto track = session->track();
-      assert(track);
-
-      auto sources = track->sources();
-
-      if ( sources.size() > 0 )
-      {
-        auto& source = sources[0];
-
-        if ( source.name() == "local" ) {
-          source_local_->send(std::move(session));
-        }
-        else if ( source.name() == "spotify" ) {
-          source_spotify_->send(std::move(session));
-        }
-        else {
-          // TODO: Error!
-        }
-      }
-      else
-      {
-        // TODO: Error!
-      }
-    }
+    void play(std::shared_ptr<player_session> session);
   public:
-    static void start(dripcore::loop* loop)
-    {
-      std::cerr << "starting local source" << std::endl;
-      source_local_.reset(new source_local(loop));
-      source_local_->start();
-
-      musicbox::dm::source_spotify spotify_settings{};
-
-      auto& username = spotify_settings.username();
-      auto& password = spotify_settings.password();
-
-      if ( !username.empty() && !password.empty() )
-      {
-        std::cerr << "starting spotify source" << std::endl;
-        source_spotify_.reset(new source_spotify(loop));
-        source_spotify_->start();
-      }
-      else
-      {
-        std::cerr << "spotify source not configured" << std::endl;
-      }
-    }
+    static void start(dripcore::loop* loop);
   private:
     static source_ptr source_local_;
     static source_ptr source_spotify_;

eeb986cfbba90640e4e4343808dc9fa843a4adda
Benny Bach <benny.bach@gmail.com>
2017-05-07 10:15:41 +0200

    Remove spotify_import tool

--- package/debian/rakefile.rb
+++ package/debian/rakefile.rb
@@ -52,7 +52,6 @@ task :dpkg do
 
   # Copy binaries.
   sh "cp ../../build/musciteer stage/usr/share/musciteer/bin/"
-  sh "cp ../../build/spotify_import stage/usr/share/musciteer/bin/"
   sh "cp ../../build/flac_rg_tagger stage/usr/share/musciteer/bin/"
   sh "cp #{Dir['../../lib/libspotify*'].first}/lib/libspotify.so* stage/usr/share/musciteer/lib/"
 

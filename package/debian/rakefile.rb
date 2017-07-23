93428f19a09d5eb9f5797e08a725d065ac4d28d5
Benny Bach <benny.bach@gmail.com>
2017-07-23 09:09:32 +0200

    Bump debian package version

--- package/debian/rakefile.rb
+++ package/debian/rakefile.rb
@@ -36,7 +36,7 @@ task :dpkg do
   end
 
   name = "musciteer"
-  version = "0.1-8"
+  version = "0.1-9"
   architecture = %x[dpkg --print-architecture].strip
   lsb_release = %x[lsb_release -sr].strip
 

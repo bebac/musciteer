9bb35bc83a2c8121752e53fd072b1769fa38f1b5
Benny Bach <benny.bach@gmail.com>
2017-07-31 07:52:16 +0200

    webclient - List tags in playlists view

--- webclient/src/components/Playlists.vue
+++ webclient/src/components/Playlists.vue
@@ -1,14 +1,46 @@
 <template>
   <div id="m-playlists">
-    Not implemented yet
+    <m-loader v-if="loading"></m-loader>
+    <div v-else>
+      <ul>
+        <li v-for="playlist in playlists" v-on:click="play(playlist)">
+          {{ playlist }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  import Loader from '@/components/Loader'
+
   export default {
+    components: {
+      'm-loader': Loader
+    },
+
     data () {
       return {
+        playlists: [],
+        loading: false
       }
+    },
+
+    methods: {
+      play: function (playlist) {
+        this.$musciteer.send({ event: 'play', data: 'pl:tag:' + playlist })
+      }
+    },
+
+    created () {
+      axios.get('/api/playlists').then(
+        response => {
+          this.playlists = response.data
+          this.loading = false
+        }
+      )
+      this.loading = true
     }
   }
 </script>
@@ -19,9 +51,26 @@
   #m-playlists
   {
     height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 
+    >div
+    {
+      color: #666;
+
+      ul
+      {
+        list-style-type: none;
+
+        li
+        {
+          display: flex;
+          padding: 0.66em 1em;
+          border-bottom: 1px solid #eee;
+
+          &:hover {
+            background-color: lighten(complement(rgba(241, 239, 236, 1)), 4%);
+          }
+        }
+      }
+    }
   }
 </style>
